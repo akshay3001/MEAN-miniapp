63be51d10c76f21592320672b2fc080cd6f34085
Mhatre <Akshay.Mhatre@FISGLOBAL.COM>
2018-02-09 01:09:59 +0530

    controller res.json fixed

--- controller/userController.js
+++ controller/userController.js
@@ -15,8 +15,11 @@ router.post('/', function (req, res) {
         },
 
         function (err, user) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            res.status(200).send(user);
+            if (err) return res.status(500).send("There was a problem adding the information to the database.")
+            else {
+              res.json(user);
+            }
+
         });
         console.log('User created successfully');
 });
@@ -24,7 +27,7 @@ router.post('/', function (req, res) {
 router.get('/', function (req, res) {
     User.find({}, function (err, users) {
         if (err) return res.status(500).send("There was a problem finding the users.");
-        res.status(200).send(users);
+        res.json(users);
     });
 });
 module.exports = router;
